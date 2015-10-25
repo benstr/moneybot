c20fa8c8222daf2c97e151d468b33a76bc988ae5
Eric Terpstra <eterps@gmail.com>
2015-10-25 15:49:59 -0500

    Removed middle column. Can click pairs

--- client/rankingTable.js
+++ client/rankingTable.js
@@ -1,66 +1,87 @@
-Template.rankingTable.rendered = function() {
-    this.subscribe('instruments');
+Template.rankingTable.rendered = function () {
+  this.subscribe('instruments');
+  this.selectedPair = new ReactiveVar('');
 };
 
 Template.rankingTable.helpers({
 
-    latestRankings: function () {
-        var sortObj = getSortObject(this.limit);
-        var rankingSeries = SeriesData.find({},{sort: sortObj});
+  latestRankings: function () {
+    var sortObj = getSortObject(this.limit);
+    var rankingSeries = SeriesData.find({}, {sort: sortObj});
 
-        if (rankingSeries) {
-
-            return rankingSeries.map( (currencySeries) => ({
-                    currencyName: currencySeries.name,
-                    latestValue: currencySeries.data[currencySeries.data.length - 1]
-                })
-            );
-
-        }
-    },
-
-    pairs: function() {
-        var sortObj = getSortObject(this.limit);
-        var rankingSeries = SeriesData.find({},{sort: sortObj}).fetch();
-
-        if (rankingSeries && rankingSeries.length) {
-
-            return getPairs(rankingSeries);
-
-        }
-    },
-
-    validPairs: function() {
-        var sortObj = getSortObject(this.limit);
-        var rankingSeries = SeriesData.find({},{sort: sortObj}).fetch();
-
-        if (rankingSeries && rankingSeries.length) {
-            var pairs = getPairs(rankingSeries);
-            return Instruments.find({ instrument: { $in : pairs } });
-        }
+    if (rankingSeries) {
+      return rankingSeries.map((currencySeries) => ({
+          currencyName: currencySeries.name,
+          latestValue: currencySeries.data[currencySeries.data.length - 1]
+        })
+      );
     }
+  },
 
+  validPairs: function () {
+    var sortObj = getSortObject(this.limit);
+    var rankingSeries = SeriesData.find({}, {sort: sortObj}).fetch();
+
+    if (rankingSeries && rankingSeries.length) {
+      var pairs = getPairs(rankingSeries);
+      return Instruments.find({instrument: {$in: pairs}});
+    }
+  },
+
+  isPairSelected: function (pair) {
+    return Template.instance().selectedPair.get() === pair.instrument ? 'selected' : '';
+  }
+
+});
+
+Template.rankingTable.events({
+
+  'click .valid-pair-item': function (e, template) {
+    var el = $(e.currentTarget);
+    var pair = el.text();
+    var pairs = pair.split('_');
+    var theHighchart = $('#container-area').highcharts();
+
+    if (template.selectedPair.get() !== pair) {
+      template.selectedPair.set(pair);
+      theHighchart.series.forEach(function (series) {
+        if (pairs.indexOf(series.name) === -1) {
+          series.hide();
+        } else {
+          series.show();
+        }
+
+      });
+    } else {
+      theHighchart.series.forEach(function (series) {
+        template.selectedPair.set('');
+        series.show();
+      });
+    }
+  }
 
 });
 
 function getSortObject(limit) {
-    var sortLimit = limit - 1;
-    var sortObj = {};
-    sortObj['data.' + sortLimit] = -1;
-    return sortObj;
+  var sortLimit = limit - 1;
+  var sortObj = {};
+  sortObj['data.' + sortLimit] = -1;
+  return sortObj;
 }
 
 function getPairs(rankingSeries) {
-    var topThree  = rankingSeries.slice(0,3);
-    var bottomThree = rankingSeries.slice(-3, rankingSeries.length);
 
-    var pairPairs = topThree.map( (topSeries) =>
-        bottomThree.map((bottomSeries) =>
-            `${topSeries.name}_${bottomSeries.name}`
-        )
-    );
+// Get the top three currencies, and bottom three currencies according to the most recent chart data
+  var topThree = rankingSeries.slice(0, 3);
+  var bottomThree = rankingSeries.slice(-3, rankingSeries.length);
 
-    var pairs = pairPairs.reduce( (a,b) => a.concat(b), []);
+// Find all possible currency pairs like TOP_BOT
+  var pairPairs = topThree.map((topSeries) =>
+    bottomThree.map((bottomSeries) =>
+      [`${topSeries.name}_${bottomSeries.name}`, `${bottomSeries.name}_${topSeries.name}`]
+    )
+  );
 
-    return pairs;
+// Flatten the array and return it to the template
+  return _.flatten(pairPairs, true);
 }
