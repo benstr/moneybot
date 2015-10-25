c5d6d1ed8e75a1656099f1f6b66e730cc7ef23c0
Charles Watson <sircharleswatson@gmail.com>
2015-10-25 08:17:44 -0400

    Use settings.json to get base URL so we can easily switch between a live account or a practice account.

--- marketData/server/oanda.js
+++ marketData/server/oanda.js
@@ -1,6 +1,6 @@
 // Oanda settings
 OANDA = {};
-OANDA.baseURL = 'https://api-fxtrade.oanda.com/v1/';
+OANDA.baseURL = Meteor.settings.OANDA_BASEURL;
 OANDA.token = Meteor.settings.OANDA_TOKEN;
 OANDA.account = Meteor.settings.OANDA_ACCOUNT;
 OANDA.header = {headers: {'Authorization': "Bearer " + OANDA.token}};
