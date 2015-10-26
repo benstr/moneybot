e076772014bc8388b0605714289481eedb13ec9c
Pete Corey <petecorey@gmail.com>
2015-10-25 22:46:51 -0400

    Cleaned up methods

--- marketData/server/methods.js
+++ marketData/server/methods.js
@@ -1,10 +1,4 @@
 Meteor.methods({
-  "getSeriesData": function(limit) {
-    console.log("LIMIT=========", limit || "no limit");
-    var series = getCurrenciesGrowthSeries(limit);
-    return series
-  },
-
   'getRatesData': function(instrument) {
     console.log(`GETTING RATE INFO FOR: ${instrument}`);
 
@@ -14,52 +8,3 @@ Meteor.methods({
     return HTTP.get(urlString, OANDA.header);
   }
 });
-
-function getCurrenciesGrowthSeries(limit) {
-  var series = [];
-  var currencyNames = getCurrencyNames();
-
-  currencyNames.forEach(function(currencyName) {
-    var accumulativeGrowth = getAccumulativeGrowthForCurrency(currencyName,limit);
-
-    series.push({
-      name: currencyName,
-      data: accumulativeGrowth
-    });
-  });
-
-  return series
-}
-
-function getCurrencyNames() {
-  var names = [];
-
-  var currencies = Currencies.find().fetch();
-
-  currencies.forEach(function(currency) {
-    names.push(currency.name)
-  });
-
-  return names;
-}
-
-function getAccumulativeGrowthForCurrency(currencyName,limit) {
-  var growth = [];
-  var growthByDate = AvgGrowths.find({currencyName: currencyName}, {
-    sort: {time: 1},
-    limit: parseInt(limit) || 10000
-  }).fetch();
-
-
-  growthByDate.forEach(function(currency) {
-    growth.push(currency.growth)
-  })
-
-  var accumulativeGrowth = 0;
-  growth.forEach(function(growthPercentage, index) {
-    accumulativeGrowth += growthPercentage
-    growth[index] = accumulativeGrowth
-  })
-
-  return growth
-}
